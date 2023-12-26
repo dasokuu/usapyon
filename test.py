ec85b24b42ffe334255c2bc6107113ace82c147d
kuuchan-code <kuuchanxn@gmail.com>
2023-12-26 22:43:26 +0900

    update

--- test.py
+++ test.py
@@ -154,7 +154,7 @@ async def style(ctx, style_id: int = None):
             return
 
     # 現在のスタイル設定を表示
-    user_style_id = user_speaker_settings.get(user_id, 3)  # 3はデフォルトのスタイルID
+    user_style_id = user_speaker_settings.get(user_id, 8)  # 8はデフォルトのスタイルID
     user_speaker, user_style_name = get_style_details(user_style_id, "デフォルト")
 
     response = f"**{ctx.author.display_name}さんのスタイル:** {user_speaker} {user_style_name} (ID: {user_style_id})"
@@ -182,12 +182,13 @@ async def server_style(ctx, style_id: int = None):
             return
 
     # 現在のデフォルトスタイル設定を表示
-    server_default_id = user_speaker_settings.get(server_id, {}).get("default", 3)  # 3はデフォルトのスタイルID
+    server_default_id = user_speaker_settings.get(server_id, {}).get(
+        "default", 8
+    )  # 3はデフォルトのスタイルID
     server_speaker, server_default_name = get_style_details(server_default_id, "デフォルト")
 
     response = f"**{ctx.guild.name}のデフォルトスタイル:** {server_speaker} {server_default_name} (ID: {server_default_id})\n"
     await ctx.send(response)
-
 
 
 @bot.event
