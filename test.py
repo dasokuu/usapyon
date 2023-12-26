1d2dc722caaf0206ea4a056a6910611a671f99f0
kuuchan-code <kuuchanxn@gmail.com>
2023-12-26 19:11:53 +0900

    update

--- test.py
+++ test.py
@@ -7,44 +7,48 @@ import re
 import io
 import os
 
-async def audio_query(text, speaker, max_retry):
+
+headers = {'Content-Type': 'application/json'}
+
+async def audio_query(text, speaker):
+    # 音声合成用のクエリを作成します。
     query_payload = {"text": text, "speaker": speaker}
     async with aiohttp.ClientSession() as session:
-        for _ in range(max_retry):
-            async with session.post("http://localhost:50021/audio_query", params=query_payload) as response:
-                if response.status == 200:
-                    query_data = await response.json()
-                    return query_data
-                elif response.status == 422:
-                    error_detail = await response.text()
-                    print(f"Unprocessable Entity: {error_detail}")
-                    break
-                await asyncio.sleep(1)
-        raise ConnectionError("Retry limit reached for audio_query")
+        async with session.post("http://localhost:50021/audio_query", headers=headers, params=query_payload) as response:
+            if response.status == 200:
+                return await response.json()
+            elif response.status == 422:
+                error_detail = await response.text()
+                print(f"処理できないエンティティ: {error_detail}")
+                return None
 
-async def synthesis(speaker, query_data, max_retry):
+async def synthesis(speaker, query_data):
+    # 音声合成を行います。
     synth_payload = {"speaker": speaker}
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'audio/wav'
+    }
     async with aiohttp.ClientSession() as session:
-        for _ in range(max_retry):
-            headers = {'Content-Type': 'application/json'}
-            async with session.post("http://localhost:50021/synthesis", headers=headers, params=synth_payload, data=json.dumps(query_data)) as response:
-                if response.status == 200:
-                    return await response.read()
-                await asyncio.sleep(1)
-        raise ConnectionError("Retry limit reached for synthesis")
+        async with session.post("http://localhost:50021/synthesis", headers=headers, params=synth_payload, data=json.dumps(query_data)) as response:
+            if response.status == 200:
+                return await response.read()
+            return None
 
-async def text_to_speech(ctx, texts, speaker=8, max_retry=1):
+async def text_to_speech(ctx, texts, speaker=8):
     if not texts:
-        await ctx.send("Please provide a message to speak.")
+        await ctx.send("読み上げるメッセージを指定してください。")
         return
     texts = re.split("(?<=！|。|？)", texts)
     for text in texts:
-        query_data = await audio_query(text, speaker, max_retry)
-        voice_data = await synthesis(speaker, query_data, max_retry)
-        audio_source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(io.BytesIO(voice_data), pipe=True))
-        if ctx.voice_client.is_playing():
-            ctx.voice_client.stop()
-        ctx.voice_client.play(audio_source)
+        query_data = await audio_query(text, speaker)
+        if query_data:
+            voice_data = await synthesis(speaker, query_data)
+            if voice_data:
+                audio_source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(io.BytesIO(voice_data), pipe=True))
+                if ctx.voice_client.is_playing():
+                    ctx.voice_client.stop()
+                ctx.voice_client.play(audio_source)
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -55,20 +59,20 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f'{bot.user.name} がDiscordに接続しました!')
 
-@bot.command(name='join', help='Connects the bot to your voice channel.')
+@bot.command(name='join', help='ボットをボイスチャンネルに接続します。')
 async def join(ctx):
     if ctx.author.voice and ctx.author.voice.channel:
         channel = ctx.author.voice.channel
         await channel.connect()
 
-@bot.command(name='leave', help='Disconnects the bot from the voice channel.')
+@bot.command(name='leave', help='ボットをボイスチャンネルから切断します。')
 async def leave(ctx):
     if ctx.voice_client:
         await ctx.voice_client.disconnect()
 
-@bot.command(name='speak', help='Reads the specified text aloud.')
+@bot.command(name='speak', help='指定されたテキストを読み上げます。')
 async def speak(ctx, *, message=None):
     await text_to_speech(ctx, message)
 
