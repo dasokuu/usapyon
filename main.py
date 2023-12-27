aea27212ad0efaf3d358cf5e0005f2e0a37dc995
kuuchan-code <kuuchanxn@gmail.com>
2023-12-27 19:11:08 +0900

    update

--- main.py
+++ main.py
@@ -194,6 +194,34 @@ async def on_message(message):
     await speech_queue.put((voice_client, message.content, style_id))
 
 
+# キューをクリアするための関数
+async def clear_speech_queue():
+    while not speech_queue.empty():
+        try:
+            speech_queue.get_nowait()
+        except asyncio.QueueEmpty:
+            continue
+        speech_queue.task_done()
+
+
+@bot.command(name="clear", help="読み上げキューをクリアし、待機状態にします。")
+async def clear(ctx):
+    global current_voice_client
+
+    # 現在の読み上げを停止する
+    if current_voice_client and current_voice_client.is_playing():
+        current_voice_client.stop()
+
+    # キューをクリアする
+    await clear_speech_queue()
+
+    # ボットのステータスを更新する
+    await bot.change_presence(activity=discord.Game(name="待機中 | !helpでヘルプ"))
+
+    # ユーザーに通知する
+    await ctx.send("読み上げを停止し、キューをクリアしました。ボットは待機中です。")
+
+
 @bot.event
 async def on_voice_state_update(member, before, after):
     # ボット自身の状態変更を無視
@@ -210,21 +238,31 @@ async def on_voice_state_update(member, before, after):
     # ボイスチャンネルに接続したとき
     if before.channel != voice_client.channel and after.channel == voice_client.channel:
         message = f"{member.display_name}さんが{after.channel.name}に入室しました。"
-        notify_style_id = speaker_settings.get(str(member.guild.id), {}).get("notify", NOTIFY_STYLE_ID)
+        notify_style_id = speaker_settings.get(str(member.guild.id), {}).get(
+            "notify", NOTIFY_STYLE_ID
+        )
         await speech_queue.put((voice_client, message, notify_style_id))
 
     # ボイスチャンネルから切断したとき
-    elif before.channel == voice_client.channel and after.channel != voice_client.channel:
+    elif (
+        before.channel == voice_client.channel and after.channel != voice_client.channel
+    ):
         message = f"{member.display_name}さんが{before.channel.name}から退出しました。"
-        notify_style_id = speaker_settings.get(str(member.guild.id), {}).get("notify", NOTIFY_STYLE_ID)
+        notify_style_id = speaker_settings.get(str(member.guild.id), {}).get(
+            "notify", NOTIFY_STYLE_ID
+        )
         await speech_queue.put((voice_client, message, notify_style_id))
 
     # ボイスチャンネルに誰もいなくなったら自動的に切断します。
     if after.channel is None and member.guild.voice_client:
         # ボイスチャンネルにまだ誰かいるか確認します。
         if not any(not user.bot for user in before.channel.members):
-            # Update the bot's status here
-            await bot.change_presence(activity=discord.Game(name="待機中 | !helpでヘルプ"))
+            # 現在の読み上げを停止する
+            if current_voice_client and current_voice_client.is_playing():
+                current_voice_client.stop()
+
+            # キューをクリアする
+            await clear_speech_queue()
             server_id = str(member.guild.id)
             if (
                 server_id in speaker_settings
@@ -237,7 +275,10 @@ async def on_voice_state_update(member, before, after):
             await member.guild.voice_client.disconnect()
 
 
-@bot.command(name="_userdefaultstyle", help="ユーザーのデフォルトスタイルを表示または設定します。使用法: !_userdefaultstyle [スタイルID]")
+@bot.command(
+    name="_userdefaultstyle",
+    help="ユーザーのデフォルトスタイルを表示または設定します。使用法: !_userdefaultstyle [スタイルID]",
+)
 async def user_default_style(ctx, style_id: int = None):
     server_id = str(ctx.guild.id)
 
@@ -272,7 +313,9 @@ async def user_default_style(ctx, style_id: int = None):
         await ctx.send(response)
 
 
-@bot.command(name="notifystyle", help="入退室通知のスタイルを表示または設定します。使用法: !notifystyle [スタイルID]")
+@bot.command(
+    name="notifystyle", help="入退室通知のスタイルを表示または設定します。使用法: !notifystyle [スタイルID]"
+)
 async def notify_style(ctx, style_id: int = None):
     server_id = str(ctx.guild.id)
 
