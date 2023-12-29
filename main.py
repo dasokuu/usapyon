528ca1b8cc764f0cf956d8596d59eb8fccd8e793
kuuchan-code <kuuchanxn@gmail.com>
2023-12-29 23:08:24 +0900

    update

--- main.py
+++ main.py
@@ -6,43 +6,36 @@ from voice import process_playback_queue
 from bot_commands import setup, setup_commands, CustomHelpCommand
 from settings import BOT_PREFIX, GAME_NAME
 
-# Initialize bot with intents and prefix
-intents = discord.Intents.default()
-intents.messages = True
-intents.guilds = True
-intents.voice_states = True
-intents.message_content = True
-bot = commands.Bot(
-    command_prefix=BOT_PREFIX, intents=intents
-)
-
-setup_commands(bot)
-setup(bot)
-
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}")
-    await bot.change_presence(activity=discord.Game(name=GAME_NAME))
-    for guild in bot.guilds:
-        bot.loop.create_task(process_playback_queue(str(guild.id)))
-
-
-@bot.event
-async def on_message(message):
-    # ボット自身のメッセージは無視
-    if message.author == bot.user:
-        return
-
-    # コマンド処理を妨げないようにする
-    await bot.process_commands(message)
-    await handle_message(bot, message)
-
-
-@bot.event
-async def on_voice_state_update(member, before, after):
-    await handle_voice_state_update(bot, member, before, after)
-
 
 if __name__ == "__main__":
+    # Initialize bot with intents and prefix
+    intents = discord.Intents.default()
+    intents.messages = True
+    intents.guilds = True
+    intents.voice_states = True
+    intents.message_content = True
+    bot = commands.Bot(command_prefix=BOT_PREFIX, intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(f"Logged in as {bot.user.name}")
+        await bot.change_presence(activity=discord.Game(name=GAME_NAME))
+        for guild in bot.guilds:
+            bot.loop.create_task(process_playback_queue(str(guild.id)))
+
+    @bot.event
+    async def on_message(message):
+        # ボット自身のメッセージは無視
+        if message.author == bot.user:
+            return
+
+        # コマンド処理を妨げないようにする
+        await bot.process_commands(message)
+        await handle_message(bot, message)
+
+    @bot.event
+    async def on_voice_state_update(member, before, after):
+        await handle_voice_state_update(bot, member, before, after)
+
+    setup(bot)
     bot.run(os.getenv("VOICECHATLOID_TOKEN"))
