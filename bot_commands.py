ed36dd3977e0a8b76876ec22d160a4391da45f71
kuuchan-code <kuuchanxn@gmail.com>
2023-12-30 00:41:38 +0900

    update

--- bot_commands.py
+++ bot_commands.py
@@ -119,18 +119,19 @@ def get_current_style_details(guild_id, user_id, type):
 def setup_commands(bot):
     @bot.command(name="style", help="スタイルを表示または設定します。")
     async def style(ctx, type: str = "user", style_id: int = None):
-        # Check if the 'type' argument is actually a digit and thus a potential style ID
+        # Determine if the 'type' argument is actually a numeric value for the style_id
         if type.isdigit():
-            # If 'type' is a digit, reassign its value to 'style_id' and set the type to 'user'
+            # If 'type' is a digit, reassign its value to 'style_id' and set the type to a default
             style_id = int(type)  # Convert the string to an integer for the style ID
             type = "user"  # Default to 'user' type if only ID is provided
 
         valid_types = ["default", "notify", "user"]
+        # Check if the type is valid if it's not overridden by a digit
         if type not in valid_types:
             await ctx.send(f"無効なタイプが指定されました。有効なタイプ: {', '.join(valid_types)}")
             return
 
-        # コードを共通化し、異なるスタイルタイプに対応
+        # Proceed with the command using the determined type and style_id
         await handle_style_command(ctx, style_id, type)
 
     @bot.command(name="join", help="ボットをボイスチャンネルに接続し、読み上げを開始します。")
