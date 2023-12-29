4c6d48c087c3db51b5d284c6ef896680cd7e2414
kuuchan-code <kuuchanxn@gmail.com>
2023-12-30 04:05:33 +0900

    update

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         for guild in bot.guilds:
             bot.loop.create_task(process_playback_queue(str(guild.id)))
 
-    tree = app_commands.CommandTree(bot)
+    # tree = app_commands.CommandTree(bot)
 
     # Define a slash command using the bot's tree attribute
     @bot.tree.command(name='hello', description='Say hello!')
