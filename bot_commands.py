38288e2c0d1cb4c09baa670265d726bd50e5c567
kuuchan-code <kuuchanxn@gmail.com>
2024-01-01 08:52:26 +0900

    update

--- bot_commands.py
+++ bot_commands.py
@@ -21,7 +21,6 @@ from discord.ui import Button, View
 ITEMS_PER_PAGE = 4  # 1ページあたりのアイテム数
 
 
-# ステップ 2: スタイル選択ビューの作成
 class StyleSelectionView(View):
     def __init__(self, speaker):
         super().__init__()
@@ -30,24 +29,29 @@ class StyleSelectionView(View):
     async def send_initial_message(self, interaction):
         message = f"**{self.speaker['name']}の利用可能なスタイル:**\n"
         for style in self.speaker["styles"]:
+            # 各スタイル選択用のボタンを動的に追加
+            # ここでユニークなcustom_idを設定します
             self.add_item(
                 Button(label=style["name"], custom_id=f"select_style_{style['id']}")
             )
         await interaction.response.send_message(content=message, view=self)
 
-    @discord.ui.button(label="Select", style=discord.ButtonStyle.secondary, custom_id="select_style")
-    async def select_style(self, interaction: discord.Interaction, button: Button):
-        try:
+    async def handle_style_selection(self, interaction: discord.Interaction, style_id):
+        # ここにスタイル選択時の処理を記述します
+        print(f"Selected style ID: {style_id}")  # デバッグメッセージ
+        update_style_setting(str(interaction.user.id), style_id)
+        await interaction.response.send_message(f"スタイルが更新されました: {style_id}")
 
-            selected_style_id = int(button.custom_id.split("_")[-1])
-            update_style_setting(str(interaction.user.id), selected_style_id)
-            await interaction.followup.send(f"スタイルが更新されました: {selected_style_id}")
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        # ここでカスタムIDをチェックし、適切なハンドラを呼び出します
+        for item in self.children:
+            if interaction.data['custom_id'] == item.custom_id:
+                style_id = int(item.custom_id.split('_')[-1])
+                await self.handle_style_selection(interaction, style_id)
+                return True  # 正常に処理されたことを示します
+        return False  # 何も一致しなかった場合
 
-        except Exception as e:
-            # エラーメッセージをログに記録
-            print(f"Error in select_style: {e}")
-            # ユーザーにフィードバックを提供
-            await interaction.followup.send("スタイルの選択中にエラーが発生しました。")
+
 
 class PaginationView(View):
     def __init__(self, speakers, page=1):
@@ -117,10 +121,11 @@ class PaginationView(View):
         else:
             await interaction.response.edit_message(content=message, view=self)
 
-    @discord.ui.button(label="Select", style=discord.ButtonStyle.secondary, custom_id="select_speaker")
+    @discord.ui.button(
+        label="Select", style=discord.ButtonStyle.secondary, custom_id="select_speaker"
+    )
     async def select_speaker(self, interaction: discord.Interaction, button: Button):
         try:
-
             selected_speaker_id = int(button.custom_id.split("_")[-1])
             view = StyleSelectionView(self.speakers[selected_speaker_id])
             await view.send_initial_message(interaction)
