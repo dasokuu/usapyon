7cb73b254189698bf603ec00948d1977dbbf3706
kuuchan-code <kuuchanxn@gmail.com>
2024-01-16 06:14:59 +0900

    update

--- commands/join.py
+++ commands/join.py
@@ -117,9 +117,7 @@ class MoveBotView(discord.ui.View):
     async def yes_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         # Edit the original message to indicate that the operation was successful or to remove the message
         try:
-            await interaction.response.edit_message(content="新しいチャンネルにボットを移動させます。", view=None)
             await normal_join_procedure(interaction, self.synth_config, self.synth_service, self.text_processor, self.bot)
-
         except Exception as e:
             logging.error(f"Error in yes_button: {e}")
         finally:
