1ea7243c328441dffdcb30cbdadddac97f367f24
kuuchan-code <kuuchanxn@gmail.com>
2024-01-17 05:27:51 +0900

    update

--- VoiceSynthEventProcessor.py
+++ VoiceSynthEventProcessor.py
@@ -21,8 +21,8 @@ def create_info_message(
     )
     return (
         f"読み上げチャンネル: <#{text_channel_id}>\n"
-        f"{user_display_name}さんの読み上げ音声: [{user[0]}] - {user[1]}\n"
-        f"入退室時等の音声（サーバー設定）: [{announcement[0]}] - {announcement[1]}\n"
+        f"{user_display_name}さんの読み上げ音声: [{user[0]}]\n"
+        f"入退室時等の音声（サーバー設定）: [{announcement[0]}]\n"
         f"__VOICEVOXを使用するにはキャラクタークレジットを記載する必要があります。__合成音声の配信や録画などはご注意ください。"
     )
 
